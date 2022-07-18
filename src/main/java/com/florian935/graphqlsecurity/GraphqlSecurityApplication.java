14f4a8355b24612a94f8641cba41a85dd89a27a1
florian935 <florian.martin63000@gmail.com>
2022-07-18 18:43:52 +0200

    Initial commit

---
+++ src/main/java/com/florian935/graphqlsecurity/GraphqlSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.florian935.graphqlsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florian935/graphqlsecurity/GraphqlSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.graphqlsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
